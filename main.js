cb02bb0268386b55e4d5fead3f5ec68f14dda618
arnavg115 <73305591+arnavg115@users.noreply.github.com>
2020-12-17 20:23:31 -0800

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,8 @@ function func(){
 function reset(){
     document.getElementById('heading').innerHTML = 0;
 }
+function countdown(){
+     var f = document.getElementById('heading').innerHTML
+    f = parseInt(f)
+    document.getElementById('heading').innerHTML= f-1
+}
